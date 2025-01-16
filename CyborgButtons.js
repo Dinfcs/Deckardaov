6b3e7ca06017c3e259785da418eb0966c9a67abe
Dinfcs <157985276+Dinfcs@users.noreply.github.com>
2025-01-16 07:53:39 -0500

    Update CyborgButtons.js

--- CyborgButtons.js
+++ CyborgButtons.js
@@ -1,36 +1,21 @@
 // ==UserScript==
-// @name         Script Principal
+// @name         Script Auxiliar
 // @namespace    http://tampermonkey.net/
-// @version      1.1
-// @description  Define y carga scripts auxiliares según la URL
+// @version      1.0
+// @description  Carga el script principal
 // @author       Tu Nombre
 // @match        *://*/*
+// @icon         https://dinfcs.github.io/Deckardaov/logo.png
 // @grant        none
 // ==/UserScript==
-
 (function() {
     'use strict';
-
-    // Lista de scripts secundarios con las URLs correspondientes
-    const scripts = [
-        {
-            urlPattern: /^https:\/\/cyborg\.deckard\.com\//,
-            scriptUrl: 'https://dinfcs.github.io/Deckardaov/DeckardScripts/CyborgButtonsQA.js'
-        },
-        {
-            urlPattern: /^https:\/\/evolve\.com\/vacation-rentals\//,
-            scriptUrl: 'https://dinfcs.github.io/Deckardaov/DeckardScripts/EvolveAddress.js'
-        }
-    ];
-
-    scripts.forEach(({urlPattern, scriptUrl}) => {
-        if (window.location.href.match(urlPattern)) {
-            fetch(scriptUrl)
-                .then(response => response.text())
-                .then(scriptContent => {
-                    eval(scriptContent); // Ejecuta el script cargado
-                })
-                .catch(error => console.error('Error al cargar el script:', error));
-        }
-    });
+    // URL del script principal
+    const principalScriptUrl = 'https://dinfcs.github.io/Deckardaov/DeckardScripts/Principal.js';
+    fetch(principalScriptUrl)
+        .then(response => response.text())
+        .then(scriptContent => {
+            eval(scriptContent); // Ejecuta el script principal cargado
+        })
+        .catch(error => console.error('Error al cargar el script principal:', error));
 })();
