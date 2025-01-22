01f36d1c29b901708582f87956e6d9f4bd97b1e5
Dinfcs <157985276+Dinfcs@users.noreply.github.com>
2025-01-22 15:06:17 -0500

    Update ProjectResourses Foating.js

--- DeckardScripts/ProjectResourses Foating.js
+++ DeckardScripts/ProjectResourses Foating.js
@@ -1,7 +1,7 @@
 // ==UserScript==
-// @name         ProjectResources Enhanced Visualization
+// @name         Project
 // @namespace    http://tampermonkey.net/
-// @version      1.16
+// @version      1.18
 // @description  Mostrar el nombre del proyecto detectado en la URL y una tabla de datos mejorada en la parte inferior de la página.
 // @author
 // @match        https://cyborg.deckard.com/listing/*/STR*
@@ -47,142 +47,199 @@
         return null;
     }
 
-    const nombreProyecto = obtenerNombreProyectoDesdeURL();
-    if (!nombreProyecto) {
-        console.error('No se pudo detectar el nombre del proyecto desde la URL.');
-        return;
-    }
+    function inicializarScript() {
+        const nombreProyecto = obtenerNombreProyectoDesdeURL();
+        if (!nombreProyecto) {
+            console.error('No se pudo detectar el nombre del proyecto desde la URL.');
+            return;
+        }
 
-    const contenedor = document.createElement('div');
-    contenedor.style.width = '100%';
-    contenedor.style.backgroundColor = '#fff';
-    contenedor.style.marginTop = '0px';
-    contenedor.style.boxShadow = '0 4px 6px rgba(0, 0, 0, 0.1)';
-    contenedor.style.borderRadius = '8px';
-    contenedor.style.fontFamily = 'Arial, sans-serif';
-    document.body.appendChild(contenedor);
+        const contenedor = document.createElement('div');
+        contenedor.style.width = '100%';
+        contenedor.style.backgroundColor = '#fff';
+        contenedor.style.marginTop = '0px';
+        contenedor.style.boxShadow = '0 4px 6px rgba(0, 0, 0, 0.1)';
+        contenedor.style.borderRadius = '8px';
+        contenedor.style.fontFamily = 'Arial, sans-serif';
+        document.body.appendChild(contenedor);
 
-    const barraTitulo = document.createElement('div');
-    barraTitulo.style.backgroundColor = '#1E90FF';
-    barraTitulo.style.color = '#fff';
-    barraTitulo.style.padding = '1px';
-    barraTitulo.style.fontSize = '16px';
-    barraTitulo.style.fontWeight = 'bold';
+        const barraTitulo = document.createElement('div');
+        barraTitulo.style.backgroundColor = '#1E90FF';
+        barraTitulo.style.color = '#fff';
+        barraTitulo.style.padding = '1px';
+        barraTitulo.style.fontSize = '16px';
+        barraTitulo.style.fontWeight = 'bold';
+        barraTitulo.style.borderTopLeftRadius = '8px';
+        barraTitulo.style.borderTopRightRadius = '8px';
+        barraTitulo.textContent = `Proyecto Detectado: ${nombreProyecto}`;
+        contenedor.appendChild(barraTitulo);
 
-    barraTitulo.style.borderTopLeftRadius = '8px';
-    barraTitulo.style.borderTopRightRadius = '8px';
-    barraTitulo.textContent = `Proyecto Detectado: ${nombreProyecto}`;
-    contenedor.appendChild(barraTitulo);
+        const contenedorDatos = document.createElement('div');
+        contenedorDatos.style.padding = '0px';
+        contenedorDatos.style.overflowX = 'auto';
+        contenedor.appendChild(contenedorDatos);
 
-    const contenedorDatos = document.createElement('div');
-    contenedorDatos.style.padding = '0px';
-    contenedorDatos.style.overflowX = 'auto';
-    contenedor.appendChild(contenedorDatos);
+        async function cargarDatos() {
+            try {
+                const response = await fetch(jsonURL);
+                if (!response.ok) {
+                    throw new Error(`Error al leer la base de datos: ${response.statusText}`);
+                }
 
-    async function cargarDatos() {
-        try {
-            const response = await fetch(jsonURL);
-            if (!response.ok) {
-                throw new Error(`Error al leer la base de datos: ${response.statusText}`);
-            }
+                const data = await response.json();
 
-            const data = await response.json();
+                const proyectoFiltrado = data.tabla.find(proyecto =>
+                    proyecto.Project.toLowerCase() === nombreProyecto.toLowerCase()
+                );
 
-            const proyectoFiltrado = data.tabla.find(proyecto =>
-                proyecto.Project.toLowerCase() === nombreProyecto.toLowerCase()
-            );
+                if (!proyectoFiltrado) {
+                    contenedorDatos.textContent = 'No se encontraron datos para el proyecto detectado.';
+                    return;
+                }
 
-            if (!proyectoFiltrado) {
-                contenedorDatos.textContent = 'No se encontraron datos para el proyecto detectado.';
-                return;
-            }
+                const table = document.createElement('table');
+                table.style.width = '100%';
+                table.style.borderCollapse = 'collapse';
+                table.style.marginTop = '0px';
 
-            const table = document.createElement('table');
-            table.style.width = '100%';
-            table.style.borderCollapse = 'collapse';
-            table.style.marginTop = '0px';
+                const headers = ['Project', 'Public Records & GIS', 'License List', 'Important Info', 'Media'];
+                const headerWidths = ['20%', '20%', '20%', '30%', '10%'];
 
-            const headers = ['Project', 'Public Records & GIS', 'License List', 'Important Info', 'Media'];
-            const headerWidths = ['20%', '20%', '20%', '30%', '10%'];
+                const thead = table.createTHead();
+                const headerRow = thead.insertRow();
+                headers.forEach((header, index) => {
+                    const th = document.createElement('th');
+                    th.textContent = header;
+                    th.style.width = headerWidths[index];
+                    th.style.backgroundColor = '#F8F8F8';
+                    th.style.color = '#333';
+                    th.style.padding = '12px';
+                    th.style.textAlign = 'left';
+                    th.style.borderBottom = '2px solid #DDD';
+                    th.style.fontSize = '14px';
+                    th.style.fontWeight = 'bold';
+                    headerRow.appendChild(th);
+                });
 
-            const thead = table.createTHead();
-            const headerRow = thead.insertRow();
-            headers.forEach((header, index) => {
-                const th = document.createElement('th');
-                th.textContent = header;
-                th.style.width = headerWidths[index];
-                th.style.backgroundColor = '#F8F8F8';
-                th.style.color = '#333';
-                th.style.padding = '12px';
-                th.style.textAlign = 'left';
-                th.style.borderBottom = '2px solid #DDD';
-                th.style.fontSize = '14px';
-                th.style.fontWeight = 'bold';
-                headerRow.appendChild(th);
-            });
+                const tbody = table.createTBody();
+                const row = tbody.insertRow();
+                headers.forEach(header => {
+                    const cell = row.insertCell();
+                    cell.style.padding = '12px';
+                    cell.style.borderBottom = '1px solid #EEE';
+                    cell.style.verticalAlign = 'top';
+                    cell.style.color = '#555';
+                    cell.style.fontSize = '12px';
 
-            const tbody = table.createTBody();
-            const row = tbody.insertRow();
-            headers.forEach(header => {
-                const cell = row.insertCell();
-                cell.style.padding = '12px';
-                cell.style.borderBottom = '1px solid #EEE';
-                cell.style.verticalAlign = 'top';
-                cell.style.color = '#555';
-                cell.style.fontSize = '12px';
-
-                if (Array.isArray(proyectoFiltrado[header])) {
-                    proyectoFiltrado[header].forEach(link => {
-                        if (link.url && (link.type === 'Image' || /\.(jpg|jpeg|png|gif)$/.test(link.url))) {
-                            const img = document.createElement('img');
-                            img.src = link.url;
-                            img.alt = 'Image';
-                            img.style.width = '100px';
-                            img.style.height = 'auto';
-                            img.style.margin = '5px';
-                            img.style.cursor = 'pointer';
-                            img.style.borderRadius = '4px';
-                            img.style.boxShadow = '0 2px 4px rgba(0, 0, 0, 0.1)';
-                            img.onclick = () => window.open(link.url, '_blank');
-                            cell.appendChild(img);
-                        } else {
-                            const a = document.createElement('a');
-                            a.href = link.url;
-                            a.textContent = link.type;
-                            a.target = '_blank';
-                            a.style.display = 'block';
-                            a.style.marginBottom = '6px';
-                            a.style.color = '#1E90FF';
-                            a.style.textDecoration = 'none';
-                            a.style.fontWeight = 'bold';
-                            a.style.transition = 'color 0.2s';
-                            a.onmouseover = () => a.style.color = '#3742fa';
-                            a.onmouseout = () => a.style.color = '#1E90FF';
-                            cell.appendChild(a);
-                        }
-                    });
-                } else {
-                    if (header === 'Important Info' && proyectoFiltrado[header]) {
-                        const paragraphs = proyectoFiltrado[header].split('\n');
-                        paragraphs.forEach(paragraph => {
-                            const p = document.createElement('p');
-                            p.textContent = paragraph;
-                            p.style.marginBottom = '10px';
-                            p.style.lineHeight = '1.6';
-                            cell.appendChild(p);
+                    if (Array.isArray(proyectoFiltrado[header])) {
+                        proyectoFiltrado[header].forEach(link => {
+                            if (link.url && (link.type === 'Image' || /\.(jpg|jpeg|png|gif)$/.test(link.url))) {
+                                const img = document.createElement('img');
+                                img.src = link.url;
+                                img.alt = 'Image';
+                                img.style.width = '100px';
+                                img.style.height = 'auto';
+                                img.style.margin = '5px';
+                                img.style.cursor = 'pointer';
+                                img.style.borderRadius = '4px';
+                                img.style.boxShadow = '0 2px 4px rgba(0, 0, 0, 0.1)';
+                                img.onclick = () => window.open(link.url, '_blank');
+                                cell.appendChild(img);
+                            } else {
+                                const a = document.createElement('a');
+                                a.href = link.url;
+                                a.textContent = link.type;
+                                a.target = '_blank';
+                                a.style.display = 'block';
+                                a.style.marginBottom = '6px';
+                                a.style.color = '#1E90FF';
+                                a.style.textDecoration = 'none';
+                                a.style.fontWeight = 'bold';
+                                a.style.transition = 'color 0.2s';
+                                a.onmouseover = () => a.style.color = '#3742fa';
+                                a.onmouseout = () => a.style.color = '#1E90FF';
+                                cell.appendChild(a);
+                            }
                         });
                     } else {
-                        cell.textContent = proyectoFiltrado[header] || '';
+                        if (header === 'Important Info' && proyectoFiltrado[header]) {
+                            const paragraphs = proyectoFiltrado[header].split('\n');
+                            paragraphs.forEach(paragraph => {
+                                const p = document.createElement('p');
+                                p.textContent = paragraph;
+                                p.style.marginBottom = '10px';
+                                p.style.lineHeight = '1.6';
+                                cell.appendChild(p);
+                            });
+                        } else {
+                            cell.textContent = proyectoFiltrado[header] || '';
+                        }
+                    }
+                });
+
+                contenedorDatos.appendChild(table);
+
+            } catch (error) {
+                console.error('Error al cargar los datos:', error);
+            }
+        }
+
+        cargarDatos();
+    }
+
+    // Ejecuta toda la función inicializarScript 2 segundos después de cargar la página
+    window.addEventListener('load', () => {
+        setTimeout(inicializarScript, 2000);
+    });
+})();
+
+
+// CODIGO PARA BOTON DE NEARBY PARCELS 
+
+(function() {
+    'use strict';
+
+    function createButton() {
+        // Crea el botón "Abrir Nparcels"
+        const button = document.createElement('button');
+        button.innerHTML = '<b>OPEN NP</b>'; // Texto en negrita
+        button.style.position = 'fixed';
+        button.style.top = '0px';
+        button.style.left = '50%';
+        button.style.transform = 'translateX(-50%)';
+        button.style.zIndex = 9999;
+        button.style.padding = '2px';
+        button.style.backgroundColor = '#093140';
+        button.style.color = 'white';
+        button.style.border = 'none';
+        button.style.fontSize = '14px';
+        button.style.cursor = 'pointer';
+        document.body.appendChild(button);
+
+        // Agrega el evento de click al botón
+        button.addEventListener('click', () => {
+            // Selecciona todos los hipervínculos de Google en la página
+            const links = document.querySelectorAll('a[href*="https://www.google.com/search?q="]');
+            links.forEach((link, index) => {
+                // Abre solo los primeros 10 hipervínculos
+                if (index < 10) {
+                    // Extrae la dirección del hipervínculo
+                    const url = new URL(link.href);
+                    const searchParams = new URLSearchParams(url.search);
+                    const address = searchParams.get('q');
+                    // Abre la dirección en Google Maps
+                    if (address) {
+                        window.open(`https://www.google.com/maps/search/${encodeURIComponent(address)}`, '_blank');
                     }
                 }
             });
-
-            contenedorDatos.appendChild(table);
-
-        } catch (error) {
-            console.error('Error al cargar los datos:', error);
-        }
+        });
     }
 
-    cargarDatos();
+    // Ejecuta la creación del botón 5 segundos después de cargar la página
+    window.addEventListener('load', () => {
+        setTimeout(createButton, 2000);
+    });
 })();
+
+
