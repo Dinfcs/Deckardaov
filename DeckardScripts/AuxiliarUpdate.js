152f5579a98d24fde4aede0a8985c762a89ff87c
Dinfcs <157985276+Dinfcs@users.noreply.github.com>
2025-01-22 17:02:45 -0500

    Add files via upload

---
+++ DeckardScripts/AuxiliarUpdate.js
@@ -0,0 +1,14 @@
+// ==UserScript==
+// @name         Deckard Scripts
+// @namespace    https://deckardtech.atlassian.net/wiki/spaces/PC/pages/2352840706/Tools+for+Cyborg
+// @version      1.1
+// @description  Several Tools implemented for improvements in the AM process
+// @author       Luis E
+// @match       *://*/*
+// @grant        none
+// @require      https://dinfcs.github.io/Deckardaov/DeckardScripts/Principal.js
+// @require      https://dinfcs.github.io/Deckardaov/DeckardScripts/NearbyParcels.js
+// @updateURL    https://dinfcs.github.io/Deckardaov/DeckardScripts/PrincipalActualizacion.js
+// @icon         https://dinfcs.github.io/Deckardaov/logo.png
+
+// ==/UserScript==
