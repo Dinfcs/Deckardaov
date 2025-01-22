a5902b3157dabf10325da52ead8069e6976108c7
Dinfcs <157985276+Dinfcs@users.noreply.github.com>
2025-01-22 11:29:38 -0500

    Update CyborgButtonsQA.js

--- CyborgButtonsQA.js
+++ CyborgButtonsQA.js
@@ -1,21 +1,48 @@
 // ==UserScript==
-// @name         Script Auxiliar
+// @name         Principal Script 
 // @namespace    http://tampermonkey.net/
-// @version      1.0
-// @description  Carga el script principal
-// @author       Tu Nombre
+// @version      1.2
+// @description  Define and load auxiliary scripts according to the URL
+// @author       Luis Escalante
 // @match        *://*/*
-// @icon         https://dinfcs.github.io/Deckardaov/logo.png
 // @grant        none
 // ==/UserScript==
+
 (function() {
     'use strict';
-    // URL del script principal
-    const principalScriptUrl = 'https://dinfcs.github.io/Deckardaov/DeckardScripts/Principal.js';
-    fetch(principalScriptUrl)
-        .then(response => response.text())
-        .then(scriptContent => {
-            eval(scriptContent); // Ejecuta el script principal cargado
-        })
-        .catch(error => console.error('Error al cargar el script principal:', error));
+
+    // Lista de scripts secundarios con las URLs correspondientes
+    const scripts = [
+        {
+            urlPattern: /^https:\/\/cyborg\.deckard\.com\//,
+            scriptUrl: 'https://dinfcs.github.io/Deckardaov/DeckardScripts/CyborgButtons.js'
+        },
+        {
+            urlPattern: /^https:\/\/evolve\.com\/vacation-rentals\//,
+            scriptUrl: 'https://dinfcs.github.io/Deckardaov/DeckardScripts/EvolveAddress.js'
+        },
+        {
+            urlPattern: /^https:\/\/www\.misterbandb\.com\//,
+            scriptUrl: 'https://dinfcs.github.io/Deckardaov/DeckardScripts/Misterb&b%20Address.js'
+        },
+        {
+            urlPattern: /^https:\/\/cyborg\.deckard\.com\/listing\/.*\/STR.*$/,
+            scriptUrl: 'https://dinfcs.github.io/Deckardaov/DeckardScripts/ProjectResourses%20Foating.js'
+        },
+        {
+            urlPattern: /^https:\/\/www\.michaelsvacationrentals\.com\//,
+            scriptUrl: 'https://dinfcs.github.io/Deckardaov/DeckardScripts/MichaelVacationRentalsAddress.js'
+        }
+    ];
+
+    scripts.forEach(({urlPattern, scriptUrl}) => {
+        if (window.location.href.match(urlPattern)) {
+            fetch(scriptUrl)
+                .then(response => response.text())
+                .then(scriptContent => {
+                    eval(scriptContent); // Ejecuta el script cargado
+                })
+                .catch(error => console.error('Error al cargar el script:', error));
+        }
+    });
 })();
