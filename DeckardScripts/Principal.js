8d726b8dcefaaa7ce1ce5c8a4e0fe6fa3f551d64
Dinfcs <157985276+Dinfcs@users.noreply.github.com>
2025-01-22 15:22:12 -0500

    Update Principal.js

--- DeckardScripts/Principal.js
+++ DeckardScripts/Principal.js
@@ -5,8 +5,6 @@
 // @description  Define and load auxiliary scripts according to the URL
 // @author       Luis Escalante
 // @match        *://*/*
-// @match        https://www.bing.com/maps*
-// @match        https://www.google.com/maps*
 // @grant        none
 // ==/UserScript==
 
