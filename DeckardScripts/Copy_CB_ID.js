f116fa0d04b6f9c46d7d549e9c7c6c2de82e2a30
DilanSalazar15 <dilan.salazar@deckard.com>
2025-01-30 14:05:09 -0500

    Update Copy_CB_ID.js

--- DeckardScripts/Copy_CB_ID.js
+++ DeckardScripts/Copy_CB_ID.js
@@ -4,7 +4,7 @@
 // @version      1.0
 // @description  Agrega un botón para copiar el identificador de la URL en páginas que contengan "/listing/"
 // @author       Luchito & Dilan
-// @match        *://*/*listing/*
+// @match        *://*/*
 // @grant        GM_setClipboard
 // ==/UserScript==
 
