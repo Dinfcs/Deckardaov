b92b54765775a9e1e38aec32c568aad2ddeb5d20
Dinfcs <157985276+Dinfcs@users.noreply.github.com>
2025-02-06 16:07:24 -0500

    Update StylesCyborg.js

--- DeckardScripts/StylesCyborg.js
+++ DeckardScripts/StylesCyborg.js
@@ -29,16 +29,28 @@
 
         addStyle(`
     /* Encabezados */
-    .cyborg-str-tool h1:not(#window_vetting_dlg *), .cyborg-str-tool h2:not(#window_vetting_dlg *), .cyborg-str-tool h3:not(#window_vetting_dlg *), .cyborg-str-tool h4:not(#window_vetting_dlg *), .cyborg-str-tool h5:not(#window_vetting_dlg *), .cyborg-str-tool h6:not(#window_vetting_dlg *):not(.pop_up_header_container *) {
+    .cyborg-str-tool h1:not(#window_vetting_dlg *):not(#vetting_data_footer *), .cyborg-str-tool h2:not(#window_vetting_dlg *):not(#vetting_data_footer *), .cyborg-str-tool h3:not(#window_vetting_dlg *):not(#vetting_data_footer *), .cyborg-str-tool h4:not(#window_vetting_dlg *):not(#vetting_data_footer *), .cyborg-str-tool h5:not(#window_vetting_dlg *):not(#vetting_data_footer *), .cyborg-str-tool h6:not(#window_vetting_dlg *):not(#vetting_data_footer *):not(.pop_up_header_container *) {
         font-weight: bold !important;
         color: #C9D82B !important; /* Verde oliva oscuro */
         font-size: 15px !important;
     }
 
     /* Resto de los botones */
-    .cyborg-str-tool button:not(#window_vetting_dlg *):not(#btn_submit_vetting_dlg):not(.fancybox-button):not([data-test-id="float-window-minimize-or-restore-btn"]):not([data-test-id="float-window-close-btn"]):not(.pop_up_header_container *),
-    .cyborg-str-tool .btn:not(#window_vetting_dlg *):not(#btn_submit_vetting_dlg):not(.fancybox-button):not([data-test-id="float-window-minimize-or-restore-btn"]):not([data-test-id="float-window-close-btn"]):not(.pop_up_header_container *),
+    .cyborg-str-tool button:not(#window_vetting_dlg *):not(#vetting_data_footer *):not(#btn_submit_vetting_dlg):not([style*="Nearby Parcels"]):not(.fancybox-button):not([data-test-id="float-window-minimize-or-restore-btn"]):not([data-test-id="float-window-close-btn"]):not(.pop_up_header_container *),
+    .cyborg-str-tool .btn:not(#window_vetting_dlg *):not(#vetting_data_footer *):not(#btn_submit_vetting_dlg):not([style*="Nearby Parcels"]):not(.fancybox-button):not([data-test-id="float-window-minimize-or-restore-btn"]):not([data-test-id="float-window-close-btn"]):not(.pop_up_header_container *),
     #btn_submit_vetting_dlg {
+        background-color: #1b95bf !important; /* azul suave */
+        color: white !important;
+        border-radius: 5px !important;
+        padding: 5px 8px !important;
+        font-size: 14px !important;
+        border: none !important;
+        transition: 0.3s ease-in-out !important;
+    }
+        /* Botones específicos */
+    #btn_record_no_matching_parcel_found,
+    #btn_record_listing_not_live,
+    #btn_open_vetting_dlg {
         background-color: #1b95bf !important; /* azul suave */
         color: white !important;
         border-radius: 0px !important;
@@ -48,7 +60,7 @@
         transition: 0.3s ease-in-out !important;
     }
 
-    /* Estilo específico para el botón de Lina */
+    /* Estilo específico para el botón de mapeo */
     #btn_map_to_selected_probable_parcel {
         margin-left: 8px !important; /* ajustar valor */
         margin-right: 8px !important; /* ajustar valor */
@@ -60,58 +72,58 @@
     }
 
     /* Estilo específico para el input */
-    .form-check-input[id="checkbox_only_show_parcels_with_associated_license"]:not(#window_vetting_dlg *) {
+    .form-check-input[id="checkbox_only_show_parcels_with_associated_license"]:not(#window_vetting_dlg *):not(#vetting_data_footer *):not([style*="Nearby Parcels"]) {
         margin-left: -2px !important; /* ajustar valor */
     }
 
     /* Estilo específico para el label */
-    .form-check-label.form-label[for="checkbox_only_show_parcels_with_associated_license"]:not(#window_vetting_dlg *) {
+    .form-check-label.form-label[for="checkbox_only_show_parcels_with_associated_license"]:not(#window_vetting_dlg *):not(#vetting_data_footer *):not([style*="Nearby Parcels"]) {
         margin-left: 18px !important; /* ajustar valor */
     }
 
     /* Estilo específico para el input de búsqueda */
-    .dash-input#input_street_number_hint:not(#window_vetting_dlg *) {
+    .dash-input#input_street_number_hint:not(#window_vetting_dlg *):not(#vetting_data_footer *):not([style*="Nearby Parcels"]) {
         margin-left: 15px !important; /* ajustar valor */
     }
 
     /* Enlaces */
-    .cyborg-str-tool a:not(#window_vetting_dlg *):not(.pop_up_header_container *) {
+    .cyborg-str-tool a:not(#window_vetting_dlg *):not(#vetting_data_footer *):not(.pop_up_header_container *) {
         color: #919292 !important; /* Gris para enlaces sin abrir */
         text-decoration: none !important;
         font-weight: normal !important;
     }
 
-    .cyborg-str-tool a:visited:not(#window_vetting_dlg *):not(.pop_up_header_container *) {
+    .cyborg-str-tool a:visited:not(#window_vetting_dlg *):not(#vetting_data_footer *):not(.pop_up_header_container *) {
         color: #23A9D8 !important; /* Azul para enlaces visitados */
     }
 
-    .cyborg-str-tool a:hover:not(#window_vetting_dlg *):not(.pop_up_header_container *) {
+    .cyborg-str-tool a:hover:not(#window_vetting_dlg *):not(#vetting_data_footer *):not(.pop_up_header_container *) {
         color: #23A9D8 !important;
         text-decoration: underline !important;
     }
 
     /* Tablas */
-    .cyborg-str-tool table:not(#window_vetting_dlg *):not(.table-hover):not(.pop_up_header_container *) {
+    .cyborg-str-tool table:not(#window_vetting_dlg *):not(#vetting_data_footer *):not(.table-hover):not(.pop_up_header_container *) {
         border-collapse: collapse !important;
         width: 100% !important;
         font-size: 12px !important;
     }
 
-    .cyborg-str-tool table th:not(#window_vetting_dlg *):not(.bg-secondary):not(.pop_up_header_container *), .cyborg-str-tool table td:not(#window_vetting_dlg *):not(.bg-secondary):not(.pop_up_header_container *) {
+    .cyborg-str-tool table th:not(#window_vetting_dlg *):not(#vetting_data_footer *):not(.bg-secondary):not(.pop_up_header_container *), .cyborg-str-tool table td:not(#window_vetting_dlg *):not(#vetting_data_footer *):not(.bg-secondary):not(.pop_up_header_container *) {
         border: 1px solid #ddd !important;
         padding: 3px !important;
         text-align: left !important;
         color: black !important; /* Cambia el color de las letras a negro */
     }
 
-    .cyborg-str-tool table th:not(#window_vetting_dlg *):not(.bg-secondary):not(.pop_up_header_container *) {
+    .cyborg-str-tool table th:not(#window_vetting_dlg *):not(#vetting_data_footer *):not(.bg-secondary):not(.pop_up_header_container *) {
         background-color: #edede8 !important; /* Verde oliva */
         color: black !important; /* Cambia el color de las letras a negro */
         font-size: 12px !important;
     }
 
     /* Estilo de los campos de filtro */
-    .cyborg-str-tool table input[type="text"]:not(#window_vetting_dlg *):not(.pop_up_header_container *) {
+    .cyborg-str-tool table input[type="text"]:not(#window_vetting_dlg *):not(#vetting_data_footer *):not(.pop_up_header_container *) {
         background-color: #eeeeee !important; /* Gris claro para campos de filtro */
         border: 1px solid #ccc !important; /* Borde gris */
         padding: 5px !important;
@@ -119,31 +131,31 @@
         width: 100% !important;
     }
 
-    .cyborg-str-tool input:focus:not(#window_vetting_dlg *):not(.pop_up_header_container *), .cyborg-str-tool select:focus:not(#window_vetting_dlg *):not(.pop_up_header_container *), .cyborg-str-tool textarea:focus:not(#window_vetting_dlg *):not(.pop_up_header_container *) {
+    .cyborg-str-tool input:focus:not(#window_vetting_dlg *):not(#vetting_data_footer *):not(.pop_up_header_container *), .cyborg-str-tool select:focus:not(#window_vetting_dlg *):not(#vetting_data_footer *):not(.pop_up_header_container *), .cyborg-str-tool textarea:focus:not(#window_vetting_dlg *):not(#vetting_data_footer *):not(.pop_up_header_container *) {
         border-color: #4a6984 !important; /* Azul suave */
         outline: none !important;
         background-color: #fff !important;
     }
 
     /* Reducir tamaño del selector de página */
-    .cyborg-str-tool .pagination input:not(#window_vetting_dlg *):not(.pop_up_header_container *) {
+    .cyborg-str-tool .pagination input:not(#window_vetting_dlg *):not(#vetting_data_footer *):not(.pop_up_header_container *) {
         width: 50px !important;
         font-size: 11px !important;
         text-align: center !important;
     }
 
     /* Espaciado general */
-    .cyborg-str-tool .container:not(#window_vetting_dlg *):not(.pop_up_header_container *), .cyborg-str-tool .content:not(#window_vetting_dlg *):not(.pop_up_header_container *) {
+    .cyborg-str-tool .container:not(#window_vetting_dlg *):not(#vetting_data_footer *):not(.pop_up_header_container *), .cyborg-str-tool .content:not(#window_vetting_dlg *):not(#vetting_data_footer *):not(.pop_up_header_container *) {
         padding: 10px !important;
     }
 
-    .cyborg-str-tool table th[data-dash-column="city_p"]:not(#window_vetting_dlg *):not(.pop_up_header_container *) {
+    .cyborg-str-tool table th[data-dash-column="city_p"]:not(#window_vetting_dlg *):not(#vetting_data_footer *):not(.pop_up_header_container *) {
         min-width: 180px !important; /* ajustar valor */
         max-width: 300px !important; /* ajustar valor */
     }
 
     /* No aplicar negrita al texto dentro de listing_quick_view_apn_or_address_info */
-    .cyborg-str-tool .listing_quick_view_apn_or_address_info:not(#window_vetting_dlg *) {
+    .cyborg-str-tool .listing_quick_view_apn_or_address_info:not(#window_vetting_dlg *):not(#vetting_data_footer *) {
         font-weight: normal !important;
     }
         `);
