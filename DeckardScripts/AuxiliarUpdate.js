3fd3a3be0d487d8857620eb7d08a0660145efc2e
Dinfcs <157985276+Dinfcs@users.noreply.github.com>
2025-02-12 15:01:11 -0500

    Add files via upload

---
+++ DeckardScripts/AuxiliarUpdate.js
@@ -0,0 +1,16 @@
+// ==UserScript==
+// @name         Deckard Scripts
+// @namespace    http://tampermonkey.net/
+// @version      1
+// @description  Several Tools implemented for improvements in the AM process
+// @author       Luis E
+// @match        *://*/*
+// @grant        none
+// @require      https://dinfcs.github.io/Deckardaov/DeckardScripts/Principal.js
+// @require      https://dinfcs.github.io/Deckardaov/DeckardScripts/ExternalScripts/UniversalAE.js
+// @updateURL    https://dinfcs.github.io/Deckardaov/DeckardScripts/AuxiliarUpdate.js
+// @downloadURL  https://dinfcs.github.io/Deckardaov/DeckardScripts/AuxiliarUpdate.js
+// @icon         https://dinfcs.github.io/Deckardaov/logo.png
+// ==/UserScript==
+
+/* Aquí va el contenido del script actualizado */
