8e7262fb07e864c30bfcdf293fb16a2f5ce00901
Dinfcs <157985276+Dinfcs@users.noreply.github.com>
2025-04-11 15:46:33 -0500

    Delete DeckardScripts/Licenses/database.js

--- DeckardScripts/Licenses/database.js
+++
@@ -1,4 +0,0 @@
-const database = {
-  "TX - City Of Galveston.json": "https://dinfcs.github.io/Deckardaov/DeckardScripts/Licenses/TX%20-%20City%20Of%20Galveston.json",
-  "MT - City of Bozeman.json": "https://dinfcs.github.io/Deckardaov/DeckardScripts/Licenses/MT%20-%20City%20of%20Bozeman.json"
-};
