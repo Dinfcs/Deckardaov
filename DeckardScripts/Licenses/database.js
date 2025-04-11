bbf29c9e0399f6f3996ee1f331238888cb293338
Dinfcs <157985276+Dinfcs@users.noreply.github.com>
2025-04-11 14:41:05 -0500

    Upload or update database.js

--- DeckardScripts/Licenses/database.js
+++ DeckardScripts/Licenses/database.js
@@ -1,1 +1,4 @@
-
+const database = {
+  "TX - City Of Galveston.json": "https://dinfcs.github.io/Deckardaov/DeckardScripts/Licenses/TX%20-%20City%20Of%20Galveston.json",
+  "Copy of TX - City Of Galveston.json": "https://dinfcs.github.io/Deckardaov/DeckardScripts/Licenses/Copy%20of%20TX%20-%20City%20Of%20Galveston.json"
+};
